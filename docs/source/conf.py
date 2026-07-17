1bdaac9bda3328c9f4912934cf766a2dd6eac16e
Fabio Giordana <fabio.giordana1@gmail.com>
2026-07-17 22:41:07 +0100

    updated version after small fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "GRID"
 author = "Fabio Giordana"
 copyright = "2026, Fabio Giordana, Zeynep Kiziltan, Ryo Kuroiwa"
-release = "0.1.0"
+release = "0.0.1"
 
 extensions = [
     "sphinx.ext.autodoc",
